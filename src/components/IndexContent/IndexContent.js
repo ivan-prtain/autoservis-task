5fad28688a7ede1b8f21d4f6bef662e7520fbd69
Ivan Prtain <ivan.prtain@hotmail.com>
2021-10-02 18:43:42 +0200

    fixed class>className

--- src/components/IndexContent/IndexContent.js
+++ src/components/IndexContent/IndexContent.js
@@ -3,10 +3,10 @@ import './IndexContent.css'
 
 const IndexContent = ({ functionality, directStyle }) => {
     return (
-        <div class='main-content' style={{ opacity: directStyle }}>
+        <div className='main-content' style={{ opacity: directStyle }}>
 
-            <div class='main-content-title'>Pritisnite gumb niže kako biste pokrenuli</div>
-            <button class='start-button' style={{ marginTop: '60px' }} onClick={functionality}>Pokreni konfigurator</button>
+            <div className='main-content-title'>Pritisnite gumb niže kako biste pokrenuli</div>
+            <button className='start-button' style={{ marginTop: '60px' }} onClick={functionality}>Pokreni konfigurator</button>
 
         </div>
     )
